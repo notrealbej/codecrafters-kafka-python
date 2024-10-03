d4a49e7149401f0697d838aff16dd2c87c65a20f
notrealbej <adthegreat80@gmail.com>
2024-10-03 19:54:45 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,12 @@ def main():
     # Uncomment this to pass the first stage
     #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    client, addr = server.accept() # wait for client
+
+    data = client.recv(1024)
+
+    server.sendall("00 00 00 0a")
+    server.sendall("00 00 00 07")
 
 
 if __name__ == "__main__":
