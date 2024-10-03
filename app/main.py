1897378d2451f87670789d5bb5a440179301690b
notrealbej <adthegreat80@gmail.com>
2024-10-04 00:22:24 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
 import socket  # noqa: F401
 import struct
 
-def create_message(id):
-    id_bytes = id.to_bytes(4, byteorder="big")
-    return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
+def create_message(coRelationID: int, errorCode: int | None = None) -> bytes:
+    message = coRelationID.to_bytes(4, byteorder="big")
+    if(errorCode != None):
+        message += errorCode.to_bytes(2, byteorder="big")
+
+    messageLen = len(message).to_bytes(4, byteorder="big")
+    return messageLen + message
 
 def handle_client(client):
     data = client.recv(1024)
     apiVersion = int.from_bytes(data[6:8], byteorder="big")
     coRelationID = int.from_bytes(data[8:12], byteorder="big")
-    client.sendall(create_message(coRelationID))
+    errorCode = None
     if(apiVersion not in [0, 1, 2, 3, 4]):
-        client.sendall(create_message(35))
+        errorCode = 35
+
+    client.sendall(create_message(coRelationID, errorCode))
     client.close()
 
 
