f286be2df6e9550d6f71c1206bc23d8e45bcd3c3
notrealbej <adthegreat80@gmail.com>
2024-10-03 21:51:40 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -5,21 +5,16 @@ def create_message(id):
     return len(id_bytes).to_bytes(4, byteorder="big") + id_bytes
 
 def handle_client(client):
-    client.recv(1024)
-    client.sendall(create_message(7))
+    data = client.recv(1024)
+    client.sendall(create_message(struct.unpack("!i", data)[1]))
     client.close()
 
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     while True:
-        client, addr = server.accept() # wait for client
+        client, addr = server.accept() 
         handle_client(client)
 
 
